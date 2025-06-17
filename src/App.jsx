529247bc4c4645330992c5d94b414670aa4714a0
avneets0419 <avneet0419@gmail.com>
2025-06-18 02:37:09 +0530

    Build resolve

--- src/App.jsx
+++ src/App.jsx
@@ -2,28 +2,23 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 
 import Blog from "./pages/Blog";
-
-
-
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import About from "./pages/About";
 import Services from "./pages/Services";
-
-
 
 const App = () => {
   return (
     <>
-      <Navbar/>
+      <Navbar />
       <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/blog" element={<Blog/>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/blog" element={<Blog />} />
         <Route path="/about" element={<About />} />
         <Route path="/services" element={<Services />} />
       </Routes>
-      <Footer/>
+      <Footer />
     </>
   );
 };
